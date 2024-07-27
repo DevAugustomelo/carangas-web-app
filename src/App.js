0286d9249de5657676b00af26be848298764e3b7
Augusto Melo <guga7msn@hotmail.com>
2024-07-27 20:49:25 -0300

    Navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './pages/home/Home';
 
 
 
@@ -6,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>CarangasCar</h1>
+    <Home />
   );
 }
 
