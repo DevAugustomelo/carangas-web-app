a2bc97d259f4e4d258da50f69197426c365c4d87
Augusto Melo <guga7msn@hotmail.com>
2024-08-28 10:39:34 -0300

    New car post method

--- api/routes/car.js
+++ api/routes/car.js
@@ -9,13 +9,27 @@ import CarSchema from "../models/car.js";
 
 const carRoute = Router();
 
-
+//get all cars
 carRoute.get('/', (req, res) => {
     CarSchema.find()
     .then(car => res.json(car))
-    .catch(error => res.status(400).res.json(`ERROR: ${error}`));
+    .catch(error => res.status(400).json(`ERROR: ${error}`));
 });
 
+
+//add new car
+carRoute.post('/add', (req, res) => {
+    const newCar = new CarSchema({
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        color: req.body.color,
+    })
+
+    newCar.save()
+    .then(() => res.json("New Car posted successfully"))
+    .catch(error => res.status(400).json(`ERROR: ${error}`));
+})
 
 
 export default carRoute;
